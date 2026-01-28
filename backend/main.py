1e6fc5a9473c2d4dd7d79f79f92953ab232848ed
Amr Gamal <amrgum2033@gmail.com>
2026-01-28 14:29:15 +0200

    feat(api): integrate trained models with backend API

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from random import randint
+import joblib
+import string
+
+MODEL, stemmer, stopwords_set, vectorizer = joblib.load("models/model.pkl")
+
+def preprocess_text(text):
+    text_corpus = []
+    text = text.lower()
+    text = text.translate(str.maketrans('', '', string.punctuation)).split()
+    text = [stemmer.stem(word) for word in text if word not in stopwords_set]
+    text = " ".join(text)
+    text_corpus.append(text)
+    x_text = vectorizer.transform(text_corpus)
+    return x_text
 
 class RequestModel(BaseModel):
     text: str = "empty"
@@ -16,11 +29,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.post("/")
+@app.get("/")
 async def index():
     return "server is running"
 
-@app.post("/scan/text")
-async def scan_text(msg: RequestModel):
-    print(msg)
-    return randint(0, 100)
+@app.get("/scan/text")
+async def scan_text():
+    text = """
+    Hello, do you want to meet today
+    """
+    X_text = preprocess_text(text)
+    prediction = MODEL.predict(X_text)
+    label = "scam" if prediction == 1 else "human"
+    proba = MODEL.predict_proba(X_text)
+    return {
+        "text": text.strip(),
+        "prediction": int(prediction[0]),
+        "label": label,
+        "prob_scam": proba[0][1],
+        "prob_not_scam": proba[0][0],
+        }
