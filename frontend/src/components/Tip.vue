97845d1950ab8b1c07b3b63ac385c42878235463
Amr Gamal <amrgum2033@gmail.com>
2026-02-01 01:10:42 +0200

    feat(ui): display tip fetched from api

--- frontend/src/components/Tip.vue
+++ frontend/src/components/Tip.vue
@@ -1,4 +1,25 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const tip = ref("");
+
+async function getTip() {
+    const today = new Date().toISOString().split("T")[0];
+    const lastFetched = localStorage.getItem("lastTipDate");
+    const savedTip = localStorage.getItem("dailyTip");
+    if (lastFetched === today && savedTip) {
+        tip.value = JSON.parse(savedTip);
+        return;
+    }
+    const response = await fetch("http://127.0.0.1:8000/get-tip");
+    const data = await response.json();
+    console.log(data["tip"]);
+    tip.value = data["tip"];
+    localStorage.setItem("dailyTip", JSON.stringify(tip.value));
+    localStorage.setItem("lastTipDate", today);
+}
+
+getTip();
+</script>
 
 <template>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -16,7 +37,7 @@
             <div class="tip-container">
                 <h2>Today's Tip</h2>
             </div>
-            <p>Never give your password to anyone!</p>
+            <p>{{ tip }}</p>
         </div>
     </section>
 </template>
@@ -56,5 +77,6 @@ p {
     font-size: 35px;
     font-weight: 500;
     font-family: "League Gothic", serif;
+    margin-left: 10%;
 }
 </style>
